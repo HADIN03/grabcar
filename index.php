75b93f4f0304fc393062fc6c9df7cb3f6743cfc9
HADIN03 <150939673+HADIN03@users.noreply.github.com>
2024-01-11 06:43:47 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,267 @@
+<?php 
+    $connection=mysqli_connect("localhost","root","","vehicle management");
+
+    session_start();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>GRAB</title>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+     <script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="./slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="./slick/slick-theme.css"> 
+   
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="animate.css">
+    <link rel="stylesheet" href="style.css">
+    
+
+ 
+</head> 
+<style>
+    
+.parallax {
+    /* The image used */
+    background-image: url("bus-people-public-transportation-34171.jpg");
+    height: 100%;
+
+    /* Set a specific height */
+    min-height: 700px; 
+
+    /* Create the parallax scrolling effect */
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    
+    }
+    
+.parallax1 {
+    /* The image used */
+    background-image: url("pexels-photo-280310 .jpeg");
+    height: 100%;
+
+    /* Set a specific height */
+    min-height: 600px; 
+
+    /* Create the parallax scrolling effect */
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    
+    } 
+    
+    .navbar-fixed-top.scrolled {
+       background-color: ghostwhite;
+      transition: background-color 200ms linear;
+    }
+
+</style>
+<body data-spy="scroll" data-target=".navbar" data-offset="50" onload="myFunction()"> 
+   
+   
+ 
+       
+    
+     
+     
+     
+    <!--
+    <div>  
+       <div class="jumbotron">
+          <h2 class="animated bounce">Ruet Vehicle Management</h2>      
+          <p>A management system where you can easily manage vehicles.</p>
+        </div>
+           
+       
+        
+    </div> 
+    -->
+    <div class="parallax foo">
+       <?php include 'navbar.php';?>
+    
+        <div class="hero-text" style="font-size:50px text-align: center; position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);color: white;">
+           
+            <h1 class="animated rubberBand" >LOGIN NEW FOR GRAB</h1>
+            <p>A management system where you can easily manage vehicles</p>
+            
+            <?php if(isset($_SESSION['username'])==true) { ?>
+            <a class="btn btn-success" style="text-align: center" href="booking.php">Book a Vehicle</a>
+            
+            <?php } else{  ?>
+            <a class="btn btn-success" style="text-align: center" href="login.php">Login To Book A Vehicle</a> 
+            <?php } ?>
+            
+          </div>
+    </div>                 
+    
+    <div>
+       <br><br>
+        <div id="bus_route" class="container">
+          <div class="page-header">
+            <h1 style="text-align: center">Advantage Services</h1>      
+          </div> 
+          <div class="row">
+              <div class="col-md-12 foo">
+                <p>&nbsp;</p>
+                <img src="Screenshot 2024-01-11 023433.png" width="1171" height="191" class="img-responsive" >  
+              </div>
+              <div class="col-md-6">
+                  <br>
+                  
+              </div>
+          </div>       
+        </div>
+        
+        <br>
+        <div class="page-header">
+            <h1 style="text-align: center">Driver</h1>      
+        </div>
+        <div class="parallax1"></div>
+        <div id="driver" class="container">
+          
+          <div class="row">
+              <div class="col-md-12">
+                  <p style="font-size: 20px;">The driver of ruet are very punctual and they provides great service. Every one of them is professional and great at their jobs</p>
+                  
+              </div>
+          </div>
+               
+        </div>
+        
+        
+        <div id="bus" class="container">
+          <div class="page-header">
+  
+          </div>
+          <img src="grab.png" width="1182" height="498" alt=""/> 
+          <div class="row">
+              <div class="col-md-6">
+                
+
+            </div>
+
+              
+          </div>       
+        </div>
+        
+        
+          
+          <p></p>      
+                
+        </div>  
+        
+        <footer style="background-color: #2f2f2f;
+          color: #fff; padding-top: 70px;
+          padding-bottom: 70px;" class="container-fluid text-center">
+          <p></p>
+		  <div class="col-md-12">
+				<div class="col-md-7 col-lg-3">&nbsp;</div>
+			<div class="col-md-6 col-lg-3 info_col">
+          <div class="info_contact">
+            <h4>
+              Address
+            </h4>
+            <div class="contact_link_box">
+              <a href="">
+                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                <span>
+                  Location
+                </span>
+              </a>
+              <a href="">
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                <span>
+                  Call +01 1234567890
+                </span>
+              </a>
+              <a href="">
+                <i class="fa fa-envelope" aria-hidden="true"></i>
+                <span>
+                grabstudent@gmail.com
+              </span>
+              </a>
+            </div>
+          </div>
+          <div class="info_social">
+            <a href="">
+              <i class="fa fa-facebook" aria-hidden="true"></i>
+            </a>
+            <a href="">
+              <i class="fa fa-twitter" aria-hidden="true"></i>
+            </a>
+            <a href="">
+              <i class="fa fa-linkedin" aria-hidden="true"></i>
+            </a>
+            <a href="">
+              <i class="fa fa-instagram" aria-hidden="true"></i>
+            </a>
+          </div>
+        </div>
+			<div class="col-md-6 col-lg-2 mx-auto info_col">
+          <div class="info_link_box">
+            <h4>
+              Links
+            </h4>
+            <div class="info_links">
+              <a class="active" href="index.php">
+                Home
+              </a>
+              <a class="" href="about.html">
+                About
+              </a>
+              <a class="" href="booking.php">
+              Order
+            </a>
+              <a class="" href="route.php">
+              Contact
+            </a>
+              <a class="" href="driverlist.php">
+                Driver
+              </a>
+            </div>
+          </div>
+        </div>
+			
+			</div>
+			
+        </footer>
+        
+        
+        
+        
+    
+    
+    
+<script>
+    $(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $a= $(".parallax");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $a.height());
+  });
+}); 
+    
+    </script>    
+  
+  
+  <script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
+  
+  
+  <script>
+        window.sr = ScrollReveal();
+        sr.reveal('.foo', { duration: 800 });
+        sr.reveal('.foo1', { duration: 800,origin: 'top'});
+    </script>
+    
+</body>
+</html>
---
+++ route.php
@@ -0,0 +1,170 @@
+<?php 
+ session_start();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>vehicle management system</title>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" type="text/css" href="./slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="./slick/slick-theme.css"> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+
+ 
+</head> 
+<style>
+    /* Center the loader 
+#loader {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  z-index: 1;
+  width: 150px;
+  height: 150px;
+  margin: -75px 0 0 -75px;
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #3498db;
+  width: 120px;
+  height: 120px;
+  -webkit-animation: spin 2s linear infinite;
+  animation: spin 2s linear infinite;
+}
+
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}   */ 
+</style>
+
+
+<body onload="myFunction()">
+    <div id="loader"></div>
+    
+    <div id="myDiv">
+      <?php include 'navbar.php';?>
+      <br>
+       <div class="page-header foo">
+            <h1 style="text-align: center;">Contact For information</h1>      
+      </div> 
+       <div class="col-md-3"></div>
+       <div class="col-md-6">
+           <p>---------------------------<img src="Screenshot 2024-01-11 025352.png" width="537" height="1203" alt=""/>---------------------------</p>
+		   
+           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15914.19793907438!2d103.46311010000001!3d4.3072776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31c87e6a776b1a21%3A0x30e7676483732b6b!2sUC%20TATI!5e0!3m2!1sms!2smy!4v1702216185640!5m2!1sms!2smy" width="810" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+           <p>&nbsp;</p>
+<div class="container">
+      <div class="row">
+        <div class="col-md-6 col-lg-3 info_col">
+          <div class="info_contact">
+            <h4>
+              Address
+            </h4>
+            <div class="contact_link_box">
+              <a href="">
+                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                <span>
+                  Location
+                </span>
+              </a>
+              <a href="">
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                <span>
+                  Call +01 1234567890
+                </span>
+              </a>
+              <a href="">
+                <i class="fa fa-envelope" aria-hidden="true"></i>
+                <span>
+                grabstudent@gmail.com
+              </span>
+              </a>
+            </div>
+          </div>
+          <div class="info_social">
+            <a href="">
+              <i class="fa fa-facebook" aria-hidden="true"></i>
+            </a>
+            <a href="">
+              <i class="fa fa-twitter" aria-hidden="true"></i>
+            </a>
+            <a href="">
+              <i class="fa fa-linkedin" aria-hidden="true"></i>
+            </a>
+            <a href="">
+              <i class="fa fa-instagram" aria-hidden="true"></i>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-3">&nbsp;</div>
+        <div class="col-md-6 col-lg-2 mx-auto info_col">
+          <div class="info_link_box">
+            <h4>
+              Links
+            </h4>
+            <div class="info_links">
+              <a class="active" href="index.php">
+                Home
+              </a>
+              <a class="" href="about.html">
+                About
+              </a>
+              <a class="" href="booking.php">
+              Order
+            </a>
+              <a class="" href="route.php">
+              Contact
+            </a>
+              <a class="" href="driverlist.php">
+                Driver
+              </a>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+      </div> 
+      <div class="col-md-3"></div>
+       <p>&nbsp;</p> 
+        
+    </div> 
+    
+<script>
+var myVar;
+
+    //for fakeloader
+function myFunction() {
+    //myVar = setTimeout(showPage, 1500);
+}
+
+function showPage() {
+  //document.getElementById("loader").style.display = "none";
+  //document.getElementById("myDiv").style.display = "block";
+}
+</script>
+
+<script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
+  
+  
+  <script>
+        window.sr = ScrollReveal();
+        sr.reveal('.foo', { duration: 800 });
+        sr.reveal('.foo1', { duration: 800,origin: 'top'});
+    </script>
+
+</body>
+</html>
